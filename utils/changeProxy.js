cd3f2e0947b9d5f48ad0d8d94f4940c0fb55391b
Pavel Ignatev <palllkaignatev@gmail.com>
2023-07-06 23:09:05 +0500

    first commit

--- utils/changeProxy.js
+++ utils/changeProxy.js
@@ -1,20 +1,27 @@
 const { default: axios } = require("axios");
 
 async function changeProxy() {
-  try {
-    const result = await axios.get(
-      "https://frigate-proxy.ru/ru/change_ip/82d68ac1341d35f48d503c735d9a6149/1014889"
-    );
-    if (result.data.includes("Слишком частая смена")) {
-      console.log("Слишком частая смена proxy. Повторный запрос...");
-      await changeProxy();
-    } else {
-      await new Promise((res) => setTimeout(res, 10000));
-      console.log(result.data);
+  let retryCount = 0;
+
+  while (retryCount < 10) {
+    try {
+      const result = await axios.get(
+        "https://frigate-proxy.ru/ru/change_ip/82d68ac1341d35f48d503c735d9a6149/1014889"
+      );
+
+      if (result.data.includes("Слишком частая смена")) {
+        console.log("Слишком частая смена proxy. Повторный запрос...");
+        retryCount++;
+        await new Promise((resolve) => setTimeout(resolve, 10000));
+      } else {
+        console.log(result.data);
+        break; // Прерываем цикл, если успешно получили данные
+      }
+    } catch {
+      console.log("Ошибка при смене прокси. Повторный запрос...");
+      retryCount++;
+      await new Promise((resolve) => setTimeout(resolve, 10000));
     }
-  } catch {
-    console.log("Ошибка при смене прокси. Повторный запрос...");
-    await changeProxy();
   }
 }
 
