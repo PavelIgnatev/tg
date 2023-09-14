c0fb757259f4ff5815d612823c5277b1fc6bd8ba
Pavel <palllkaignatev@gmail.com>
2023-09-14 19:06:14 +0300

    Push

--- helpers/initialBrowser.js
+++ helpers/initialBrowser.js
@@ -18,20 +18,6 @@ const initialBrowser = async (headless, username) => {
 
   const browser = await chromium.launch({
     headless: headless,
-    ignoreDefaultArgs: [
-      "--enable-automation",
-      "--disable-extensions",
-      "--disable-plugins",
-      "--disable-extensions-file-access-check",
-      "--disable-site-isolation-trials",
-      "--no-sandbox",
-      "--disable-setuid-sandbox",
-      "--disable-dev-shm-usage",
-    ],
-    defaultViewport: null,
-    userDataDir: null,
-    devtools: false,
-    ignoreHTTPSErrors: true,
   });
 
   const context = await browser.newContext({
