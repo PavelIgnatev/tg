fd9e47eebaa928bf2ca952953989fe582958942a
Pavel <palllkaignatev@gmail.com>
2023-09-28 06:44:44 +0300

    first commit

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ const main = async (accountId) => {
     throw new Error("Произошла ошибка, accountId не был передан");
   }
 
-  const [context, browser] = await initialBrowser(false, accountId);
+  const [context, browser] = await initialBrowser(true, accountId);
   const page = await createPage(context, accountId);
 
   try {
