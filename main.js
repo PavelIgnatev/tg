4c43fa6f5fae81a8a10529ab4b631d337bdc41a3
Pavel <palllkaignatev@gmail.com>
2023-10-25 02:59:26 +0500

    Init

--- main.js
+++ main.js
@@ -69,7 +69,7 @@ const main = async (accountId, proxy) => {
 
 const startMainLoop = async () => {
   const proxy = parseArgs(process.env.args);
-  const threadCount = 3;
+  const threadCount = 1;
   const promises = [];
 
   await changeProxy(proxy.changeUrl);
