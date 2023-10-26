f0847c66d363b473f854dbdfc13b17883ec633fe
Pavel <palllkaignatev@gmail.com>
2023-10-26 15:14:56 +0500

    first commit

--- utils/makeRequestGPT.js
+++ utils/makeRequestGPT.js
@@ -4,8 +4,9 @@ function filterText(text) {
   const filteredText = text.replace(/[.\[\]$@%!#^&*+\\|<>\/{}]/g, "");
   const regex =
     /[\u{1F600}-\u{1F64F}\u{1F300}-\u{1F5FF}\u{1F680}-\u{1F6FF}\u{2600}-\u{26FF}\u{2700}-\u{27BF}\u{1F900}-\u{1F9FF}\u{1F1E0}-\u{1F1FF}]/gu;
-
-  return filteredText.replace(regex, "");
+  const regex2 =
+    /((http|https|www):\/\/.)?([a-zA-Z0-9'\/\.\-])+\.[a-zA-Z]{2,5}([a-zA-Z0-9\/\&\;\:\.\,\?\\=\-\_\+\%\'\~]*)/g;
+  return filteredText.replace(regex, "").replace(regex2, "");
 }
 
 async function makeRequestGPT(dialogue) {
