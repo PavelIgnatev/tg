fb5a622b33880902ed2df59ea116640b9d5e4643
Pavel <palllkaignatev@gmail.com>
2023-10-27 04:14:23 +0500

    first commit

--- helpers/createPage.js
+++ helpers/createPage.js
@@ -4,23 +4,17 @@ const createPage = async (context, username) => {
   if (!context) {
     throw new Error("Произошла ошибка, context не был передан");
   }
-  const {
-    localStorage: localStorageData,
-    banned = false,
-    defaultLocalStorage,
-  } = (await readAccount(username)) ?? {};
+  const { localStorage: localStorageData } =
+    (await readAccount(username)) ?? {};
 
   const page = await context.newPage();
 
+  // if banned - initial
   page.on("domcontentloaded", async () => {
     try {
       await page.evaluate((data) => {
         for (let key in data) {
-          if (banned && defaultLocalStorage) {
-            defaultLocalStorage.setItem(key, data[key]);
-          } else {
-            localStorage.setItem(key, data[key]);
-          }
+          localStorage.setItem(key, data[key]);
         }
       }, localStorageData);
     } catch {}
