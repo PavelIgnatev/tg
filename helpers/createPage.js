49688b8f876e4131d01de255674353e5e9d455a1
Pavel <palllkaignatev@gmail.com>
2023-10-28 04:59:24 +0500

    ssa

--- helpers/createPage.js
+++ helpers/createPage.js
@@ -4,19 +4,29 @@ const createPage = async (context, username) => {
   if (!context) {
     throw new Error("Произошла ошибка, context не был передан");
   }
-  const { localStorage: localStorageData } =
-    (await readAccount(username)) ?? {};
+  const {
+    localStorage: localStorageData,
+    banned,
+    defaultLocalStorage,
+  } = (await readAccount(username)) ?? {};
 
   const page = await context.newPage();
 
-  // if banned - initial
   page.on("domcontentloaded", async () => {
     try {
-      await page.evaluate((data) => {
-        for (let key in data) {
-          localStorage.setItem(key, data[key]);
-        }
-      }, localStorageData);
+      if (banned && defaultLocalStorage) {
+        await page.evaluate((data) => {
+          for (let key in data) {
+            localStorage.setItem(key, data[key]);
+          }
+        }, defaultLocalStorage);
+      } else {
+        await page.evaluate((data) => {
+          for (let key in data) {
+            localStorage.setItem(key, data[key]);
+          }
+        }, localStorageData);
+      }
     } catch {}
   });
 
