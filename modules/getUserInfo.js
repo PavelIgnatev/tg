ac08c14921828fd7cffe20ff985354a6fa6e80fd
Pavel <palllkaignatev@gmail.com>
2023-11-02 23:42:10 +0500

    ssag

--- modules/getUserInfo.js
+++ modules/getUserInfo.js
@@ -2,7 +2,7 @@ const getUserInfo = async (page) => {
   await page.waitForTimeout(3000);
 
   try {
-    const userInfo = await page.waitForSelector(".chat-info-wrapper", {
+    const userInfo = await page.waitForSelector(".user-title", {
       timeout: 3000,
     });
     await userInfo.click();
