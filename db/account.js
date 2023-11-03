72b31fd8990b6b97b4c8343fab1992facc02e9fd
Pavel <palllkaignatev@gmail.com>
2023-11-03 08:39:23 +0500

    ssa

--- db/account.js
+++ db/account.js
@@ -211,7 +211,7 @@ class AccountService {
     const unprocessedUsers = await this.collection
       .aggregate([
         // { $match: { banned: { $ne: true } } },
-        { $match: { server } },
+        { $match: { server, fullBanned: { $ne: true } } },
         {
           $group: {
             _id: "$username",
