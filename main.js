8a1ea1c1b04c755b0a3c203f2bf03773d3dba457
Pavel <palllkaignatev@gmail.com>
2023-11-22 16:10:41 +0400

    first commit

--- main.js
+++ main.js
@@ -75,7 +75,7 @@ const main = async (accountId, proxy) => {
 
 const startMainLoop = async () => {
   const proxy = parseArgs(process.env.args);
-  const threadCount = proxy.server === "yproxy.site:14553" ? 1 : 3;
+  const threadCount = 3;
   const promises = [];
 
   await changeProxy(proxy.changeUrl);
