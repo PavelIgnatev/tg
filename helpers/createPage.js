2fc35d7bf20eb4657453d6b15328465f438e0352
Pavel <palllkaignatev@gmail.com>
2023-11-25 20:23:54 +0400

    Init

--- helpers/createPage.js
+++ helpers/createPage.js
@@ -4,21 +4,15 @@ const createPage = async (context, username) => {
   if (!context) {
     throw new Error("Произошла ошибка, context не был передан");
   }
-  const {
-    localStorage: localStorageData,
-    defaultLocalStorage,
-    forceBanned,
-  } = (await readAccount(username)) ?? {};
+  const { defaultLocalStorage } = (await readAccount(username)) ?? {};
 
   const page = await context.newPage();
-
-  const ls = forceBanned ? defaultLocalStorage : localStorageData;
 
   await page.addInitScript((localStorageData) => {
     for (let key in localStorageData) {
       localStorage.setItem(key, localStorageData[key]);
     }
-  }, ls);
+  }, defaultLocalStorage);
 
   return page;
 };
