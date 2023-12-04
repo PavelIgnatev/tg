c8700b1aec1a438cdfd6383d2c2b2659e60c941e
Pavel <palllkaignatev@gmail.com>
2023-12-04 15:23:46 +0400

    Init

--- updater.js
+++ updater.js
@@ -2,25 +2,13 @@ const {
   getAllUsernames,
   getServerCounts,
   readAccounts,
-  resetAllBannedFields,
 } = require("./db/account");
 
-getAllUsernames().then((e) => console.log(e.length));
 getServerCounts().then(console.log);
-// readAccounts().then((e) => {
-//   // console.log(e.slice(126).length)
-//   e.forEach((k) => {
-//     // console.log(k.messageCount);
-//     // if (k.banned) {
-
-//     console.log(
-//       k.username,
-//       Boolean(k.forceBanned) || Boolean(k.fullBanned),
-//       k.messageCount
-//     );
-//     // }
-//   });
-// });
-// // 1
-
-resetAllBannedFields()
+getAllUsernames();
+readAccounts().then(async (e) => {
+  await new Promise((res) => setTimeout(res, 15000));
+  e.forEach((k) => {
+    console.log(k.username, Boolean(k.fullBanned), k.messageCount);
+  });
+});
