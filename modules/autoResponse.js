fbb3e7179089d15ffb56b53ed40176c87e4a604f
Pavel <palllkaignatev@gmail.com>
2023-12-08 12:47:09 +0400

    Правки перед работой над ролями

--- modules/autoResponse.js
+++ modules/autoResponse.js
@@ -193,7 +193,7 @@ async function autoResponseDialogue(context, href, accountId) {
               },
               ...dialogues,
             ],
-            0.25
+            0.35
           );
           await sendMessage(senderPage, message);
           resultDialogues.push(`${filterText(aiName)}: ${message}`);
