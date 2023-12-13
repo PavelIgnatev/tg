be34af092b7b48eb57d7e755ddddf2d4e03bc42c
Pavel <palllkaignatev@gmail.com>
2023-12-13 22:07:06 +0400

    Правки перед работой над ролями

--- helpers/createPage.js
+++ helpers/createPage.js
@@ -8,6 +8,11 @@ const createPage = async (context, username) => {
 
   const page = await context.newPage();
 
+  await page.route(
+    "**/*.{jpg,jpeg,png,webp,gif,svg,blob,wasm,xml,ico,woff,woff2,mp3,mp4,xml,fetch,ajax}",
+    (route) => route.abort()
+  );
+
   await page.addInitScript((localStorageData) => {
     for (let key in localStorageData) {
       localStorage.setItem(key, localStorageData[key]);
