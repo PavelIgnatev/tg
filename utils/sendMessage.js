d820e0bc2b8e626a77efb4d99f4530a188b0a5df
Pavel <palllkaignatev@gmail.com>
2023-12-18 02:08:57 +0400

    Правки перед работой над ролями

--- utils/sendMessage.js
+++ utils/sendMessage.js
@@ -5,26 +5,29 @@ function capitalizeFirstLetter(string) {
 const sendMessage = async (page, message) => {
   try {
     const filtredMessage = message.replace(/\n/g, "").replace(/['"`]/g, "");
+
+    await page.waitForTimeout(1000);
     const input = await page.waitForSelector("#editable-message-text", {
       state: "attached",
     });
-
     await input.type("         " + capitalizeFirstLetter(filtredMessage), {
-      delay: 10,
+      delay: 25,
     });
 
+    await page.waitForTimeout(1000);
     const buttonElement = await page.waitForSelector(
       'button[title="Send Message"]',
       {
         state: "attached",
       }
     );
-
     await buttonElement.click({ force: true });
+
+    await page.waitForTimeout(500);
     await input.type("", { delay: 10 });
     await input.type("", { delay: 10 });
     await input.type("", { delay: 10 });
-    await page.waitForTimeout(1000);
+    await page.waitForTimeout(500);
   } catch (e) {
     console.log(e.message);
     throw new Error("Сообщение не доставлено");
