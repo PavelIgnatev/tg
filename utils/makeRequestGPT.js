88e7735b2e0f1fd9de9ed93c741ab0cc1b58eed6
Pavel <palllkaignatev@gmail.com>
2023-12-25 17:56:05 +0400

    Обновление .gitignore

--- utils/makeRequestGPT.js
+++ utils/makeRequestGPT.js
@@ -19,7 +19,12 @@ async function makeRequestGPT(
         /((?:(http|https|Http|Https|rtsp|Rtsp):\/\/(?:(?:[a-zA-Z0-9\$\-\_\.\+\!\*\'\(\)\,\;\?\&\=]|(?:\%[a-fA-F0-9]{2})){1,64}(?:\:(?:[a-zA-Z0-9\$\-\_\.\+\!\*\'\(\)\,\;\?\&\=]|(?:\%[a-fA-F0-9]{2})){1,25})?\@)?)?((?:(?:[a-zA-Z0-9][a-zA-Z0-9\-]{0,64}\.)+(?:(?:aero|arpa|asia|a[cdefgilmnoqrstuwxz])|(?:biz|b[abdefghijmnorstvwyz])|(?:cat|com|coop|c[acdfghiklmnoruvxyz])|d[ejkmoz]|(?:edu|e[cegrstu])|f[ijkmor]|(?:gov|g[abdefghilmnpqrstuwy])|h[kmnrtu]|(?:info|int|i[delmnoqrst])|(?:jobs|j[emop])|k[eghimnrwyz]|l[abcikrstuvy]|(?:mil|mobi|museum|m[acdghklmnopqrstuvwxyz])|(?:name|net|n[acefgilopruz])|(?:org|om)|(?:pro|p[aefghklmnrstwy])|qa|r[eouw]|s[abcdeghijklmnortuvyz]|(?:tel|travel|t[cdfghjklmnoprtvwz])|u[agkmsyz]|v[aceginu]|w[fs]|y[etu]|z[amw]))|(?:(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9])\.(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9]|0)\.(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9]|0)\.(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[0-9])))(?:\:\d{1,5})?)(\/(?:(?:[a-zA-Z0-9\;\/\?\:\@\&\=\#\~\-\.\+\!\*\'\(\)\,\_])|(?:\%[a-fA-F0-9]{2}))*)?(?:\b|$)/gi;
       const message = data.replace("\n", "").replace(pattern, "");
 
-      if (message.includes("[") || message.includes("]") || message.includes("{") || message.includes("}")) {
+      if (
+        message.includes("[") ||
+        message.includes("]") ||
+        message.includes("{") ||
+        message.includes("}")
+      ) {
         console.log(
           `\x1b[4mПотенциальное сообщение:\x1b[0m \x1b[36m${message}\x1b[0m`
         );
@@ -108,7 +113,37 @@ async function makeRequestGPT(
           .replace("здравствуй", "")
           .replace("доброе утро", "")
           .replace("добрый вечер", "")
-          .replace("добрый день", "");
+          .replace("добрый день", "")
+
+          .replace("Hi, ", "")
+          .replace("Hi! ", "")
+          .replace("Hi!", "")
+          .replace("Hi", "")
+          .replace("hi", "")
+
+          .replace("Hello, ", "")
+          .replace("Hello! ", "")
+          .replace("Hello!", "")
+          .replace("Hello", "")
+          .replace("hello", "")
+
+          .replace("Good morning, ", "")
+          .replace("Good morning! ", "")
+          .replace("Good morning!", "")
+          .replace("Good morning", "")
+          .replace("good morning", "")
+
+          .replace("Good evening, ", "")
+          .replace("Good evening! ", "")
+          .replace("Good evening!", "")
+          .replace("Good evening", "")
+          .replace("good evening", "")
+
+          .replace("Good afternoon, ", "")
+          .replace("Good afternoon! ", "")
+          .replace("Good afternoon!", "")
+          .replace("Good afternoon", "")
+          .replace("good afternoon", "");
       }
 
       return message;
